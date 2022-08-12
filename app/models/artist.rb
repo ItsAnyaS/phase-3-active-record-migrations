8c44c7695d3a3fb704f447519eb18cedf721303c
anastasia <anyasirman@outlook.com>
2022-08-12 08:59:45 -0400

    partial sollution

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,1 +1,15 @@
 # add the Artist class here
+class CreateArtists < ActiveRecord::Migration[6.1]
+    def change
+      create_table :artists do |t|
+        t.string :name
+        t.string :genre
+        t.integer :age
+        t.string :hometown
+      end
+    end
+  end
+
+class Artist < ActiveRecord::Base
+
+end
